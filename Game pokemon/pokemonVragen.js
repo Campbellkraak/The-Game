e9c67ea3e5075c62094b0c6ad13c797ed8bcc7e9
campbell kraak <campbell.kraak@gmail.com>
2018-09-28 09:01:56 +0200

    pokemon game

---
+++ Game pokemon/pokemonVragen.js
@@ -0,0 +1,51 @@
+ document.body.style.backgroundImage = "url('pokemon.png')";
+
+
+
+setTimeout(function(){ 
+	alert('Welkom in Vermilion City.'); 
+	if (prompt('Wilt u uw pokemon healen?') === 'ja'){
+
+	alert('Welkom in de pokecenter. Dit is de plek om je pokemon te healen. Je pokemon wordt nu gehealt.')
+
+	alert('U bent nu in de pokecenter dit is de plek om je pokemon te healen. 1...2...3... je pokemon is gehealt.')
+	alert('Ga naar de gym om je tegenstanders te verslaan en bekend te worden.')
+	}else if (prompt('Wilt u naar het bos gaan om pokemon te vangen?') === 'ja'){
+
+	alert('je loopt het bos in en ineens komt Team Rocket te voorschijn. Ze verslaan je en stelen je geld.');
+	
+	alert('GAME OVER! Team Rocket heeft je verslagen!');
+
+	}else if (prompt('Wil je proffesor Oak bezoeken? Hij heeft belangerijke informatie voor je!') === 'ja'){
+
+	}if (prompt('Proffesor Oak: Bezoek een gym om bekend te worden in deze stad!') === 'ja'){
+	
+	}else if (prompt('Mensen zien je als een lafaard omdat je te bang was om naar de gym te gaan. Je bent verbannen uit Vermilion City.') === '.'){
+
+	alert('GAME OVER! Omdat je bent verbannen uit Vermilion City moet je nu eenzaam in het bos leven.');
+
+	}if (prompt('Je pokemon zijn gehealt en je wint de laatste battle tegen Misty. Wil je terug gaan naar je familie?') === 'ja'){
+
+	}if(prompt('Je komt na 1 maand lang reizen terug bij je familie en leeft nog lang en gelukkig.') === 'ja'){
+
+	}else if (prompt('Je bent gym leader maar je wordt verslagen. Je wordt verbannen uit Vermilion City') === '.');
+
+	alert('GEFELICITEERD! U HEEFT EEN IPHONE X GEWONNEN! Vul uw creditcard gegevens in om de iphone X in ontvangst te nemen!')
+
+
+	
+	
+
+	
+	
+	
+
+	
+
+
+    
+	
+	
+
+
+}, 50);
